f0a5c954e28f4ca059e3602159337e85dbb3aa02
sauravtom <stomatrix@gmail.com>
2014-03-19 17:18:29 +0530

    commented some code and added pause on start

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ var height = 500;
 var pipe_velocity = 200;    
 var game = new Phaser.Game(width, height, Phaser.AUTO, 'game_div');
 var game_state = {};
-var pipes_incoming = false;
-var flag = false;
-var count = 0;
-var correct_option;
-var bird_position;
-var game_started = false;
 
+var pipes_incoming = false,
+    flag = false,
+    count = 0,
+    correct_option,
+    bird_position,
+    game_started = false;
+
+/*Add questions and answers here !!*/
 var data_string ="\
 Question,Option One,Option Two;\
 The child,Das Kind,Die Kind;\
@@ -25,19 +27,17 @@ Fahrt,Trip,Fart;\
 Warum,Why,Warm \
 ";
 
-
-var Bdata_arr = data_string.split(';');
+var temp = data_string.split(';');
 var data_arr =[]
-for(i=0;i<Bdata_arr.length;i++){
-    data_arr.push(Bdata_arr[i].split(","));
+for(i=0;i<temp.length;i++){
+    data_arr.push(temp[i].split(","));
 }
-//console.log(data_arr);
+
 game_state.main = function() { };
 game_state.main.prototype = {
    
     preload: function() {
         this.game.stage.backgroundColor = '#71c5cf';
-        //this.game.load.spritesheet('bird', 'assets/birdie.png', 24, 24);
         this.game.load.image('bird', 'assets/bird.png');  
         this.game.load.image('pipe', 'assets/pipe.png');
         this.game.load.image('ground', 'assets/ground.png');
@@ -49,46 +49,44 @@ game_state.main.prototype = {
     },
 
     create: function() { 
-        this.bird = this.game.add.sprite(100, 245, 'bird');
-        this.bird.body.gravity.y = 1000; 
+        this.bird = this.game.add.sprite(100, 105, 'bird');
         this.bird.anchor.setTo(-0.2, 0.5);
         
         var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         space_key.onDown.add(this.jump, this); 
 
-        //this.pipes = game.add.group();
-        //this.pipes.createMultiple(20, 'pipe');
+        //add the pipes
         this.pipe = this.game.add.sprite(-width,height/2,'pipe');
 
         // Add ground tiles
         this.ground = game.add.tileSprite(0, game.world.height - 32, game.world.width, 32, 'ground');
         this.ground.tileScale.setTo(2, 2);
         
-        // Timer that calls 'add_row_of_pipes' ever 1.5 seconds
-        this.timer = this.game.time.events.loop(4000, this.add_row_of_pipes, this);
+        // Timer that calls 'add_pipe' ever 4 seconds
+        this.timer = this.game.time.events.loop(4000, this.add_pipe, this);
         
         // Add a score label on the top left of the screen
         this.score = 0;
         var style = { font: "30px Arial", fill: "#ffffff" };
         this.label_score = this.game.add.text(20, 20, "0", style);
         
-        //
-        this.text1 = this.add.text(width/2-200, height/2-50, "Click Anywhere to Start game\n Use spacebar to flap", style);
-        this.text2 = this.add.text(6*width, height/2, "Question", style);
-        this.text3 = this.add.text(7*width, height-90, "Option 2", style);
+        //Add the text boxes for question and answers
+        this.option1_tb = this.add.text(width/2-200, height/2-50, "Press Space Key to start the Game", style);
+        this.question_tb = this.add.text(6*width, height/2, "", { font: "30px Arial", fill: "#ffffff" });
+        this.option2_tb = this.add.text(7*width, height-90, "", style);
 
-        this.text5 = this.add.text(width, height/2-20, "Correct", { font: "20px Arial", fill: "#ffffff" });
+        this.result_tb = this.add.text(width, height/2-20, "Correct", { font: "20px Arial", fill: "#ffffff" });
         
         this.flap = game.add.audio('flap',0.1);
-        this.point = game.add.audio('point',0.2);
-        this.nope = game.add.audio('nope',0.2);
-        this.die = game.add.audio('die',0.2);
-
-        this.game.input.onTap.addOnce(this.start,this);
+        this.point = game.add.audio('point',0.1);
+        this.nope = game.add.audio('nope',0.1);
+        this.die = game.add.audio('die',0.1);
+        
+        //Start the game on pressing the space key
+        space_key.onDown.addOnce(this.start, this);
     },
     start: function(){
         game_started = true;
-        console.log('game started');
         this.bird.body.gravity.y = 1000;
     },
     // This function is called 60 times per second
@@ -99,19 +97,17 @@ game_state.main.prototype = {
             this.game_over(); 
         this.game.physics.overlap(this.bird, this.pipe, this.game_over, null, this);
         this.game.physics.overlap(this.bird, this.ground, this.game_over, null, this);
-        if(this.bird.position.y < height/2){
-            bird_position = 'up';
-        }
-        else{
-            bird_position = 'down';
-        }
-        if(this.text1.position.x < 100 && flag == true)
+        
+        if(this.bird.position.y < height/2){    bird_position = 'up'; }
+        else{ bird_position = 'down'; }
+        
+        if(this.option1_tb.position.x < 100 && flag == true)
             this.show_result(correct_option,bird_position);
         
         if (pipes_incoming == true)
-            this.text1.x -= pipe_velocity/60;
-            this.text2.x -= pipe_velocity/60;
-            this.text3.x -= pipe_velocity/60;
+            this.option1_tb.x -= pipe_velocity/60;
+            this.question_tb.x -= pipe_velocity/60;
+            this.option2_tb.x -= pipe_velocity/60;
         
         this.ground.tilePosition.x -= pipe_velocity/160;
         if (this.bird.angle < 20)
@@ -120,8 +116,10 @@ game_state.main.prototype = {
     },
     game_over: function(){
         this.die.play();
+        game_started = false;
         this.restart_game();
-        this.game.input.onTap.addOnce(this.start,this);
+        var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        space_key.onDown.addOnce(this.start, this);
     },
     jump: function() {
         // Add a vertical velocity to the bird
@@ -135,7 +133,7 @@ game_state.main.prototype = {
         this.game.state.start('main');
     },
 
-    add_row_of_pipes: function() {
+    add_pipe: function() {
         if(game_started == false)
             return 0;
         var r = Math.floor(100 + Math.random()*60); 
@@ -144,44 +142,44 @@ game_state.main.prototype = {
         this.pipe.reset(width,r,2);
         this.pipe.body.velocity.x = -pipe_velocity;
         
-        this.text2.content = data_arr[count][0];
-        this.text1.content = data_arr[count][n+1];
+        this.question_tb.content = data_arr[count][0];
+        this.option1_tb.content = data_arr[count][n+1];
         if(n ==0){
-            this.text3.content = data_arr[count][2];
+            this.option2_tb.content = data_arr[count][2];
             correct_option = 'up';
         } 
         else{
-            this.text3.content = data_arr[count][1];
+            this.option2_tb.content = data_arr[count][1];
             correct_option = 'down';
         }
         
         count = count >= data_arr.length-1 ? 0 : count+1;
         pipes_incoming = true;
         flag = true;
-        this.text1.position.x = width;
-        this.text2.position.x = width+80;
-        this.text3.position.x = width;
-        this.text1.position.y = r/2-20;
-        this.text2.position.y = r+100-20;
-        this.text3.position.y = (height+200+r)/2 -30;
+        this.option1_tb.position.x = width;
+        this.question_tb.position.x = width+80;
+        this.option2_tb.position.x = width;
+        this.option1_tb.position.y = r/2-20;
+        this.question_tb.position.y = r+100-20;
+        this.option2_tb.position.y = (height+200+r)/2 -30;
     },
      show_result:function(op,bp) {
         flag = false;
         if(op == bp){
             this.score+=1;
             this.label_score.content = this.score;
-            this.text5.content = "Correct";
+            this.result_tb.content = "Correct";
             this.point.play();
         }
         else{
-            this.text5.content = "Wrong";
+            this.result_tb.content = "Wrong";
             this.nope.play();
         }
-        this.game.add.tween(this.text5).to({x:width/2}, 100).to({x:width/2}, 1000).to({x:width}, 100).start();
+        this.game.add.tween(this.result_tb).to({x:width/2}, 100).to({x:width/2}, 1000).to({x:width}, 100).start();
         //console.log(op,bp);
-        this.text1.x  = -width;
-        this.text2.x  = -width;
-        this.text3.x  = -width;
+        this.option1_tb.x  = -width;
+        this.question_tb.x  = -width;
+        this.option2_tb.x  = -width;
     },
 };
 
